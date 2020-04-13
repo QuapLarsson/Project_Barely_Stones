446e799e3cb632ab53ba34e35e59b8d98a229f49
Hahoolah <oskar@kylvag.com>
2020-04-13 18:06:37 +0200

    Update BattleManager.cs

--- Assets/Scripts/Battle System/BattleManager.cs
+++ Assets/Scripts/Battle System/BattleManager.cs
@@ -7,7 +7,12 @@ public class BattleManager : MonoBehaviour
     public Fighter myFighter0;
     public Fighter myFighter1;
 
-    bool myActiveFighter; //False = 0, True = 1
+    //TODO: Abilities
+    //TODO: Critical Hit
+    //TODO: Deal Damage
+    //TODO: Animations (Hops and Shakes)
+    //TODO: UI Elements (HP, EXP, Abilities)
+    //TODO: make damage rate change depending on how much larger/smaller power is than damage
 
     // Start is called before the first frame update
     void Start()
@@ -18,133 +23,110 @@ public class BattleManager : MonoBehaviour
     //Take in two fighters and start the battle. Entry point for battles.
     public void Init(Fighter aFighter0, Fighter aFighter1)
     {
-        myActiveFighter = false;
         myFighter0 = aFighter0;
         myFighter1 = aFighter1;
 
         //TODO: Shift camera and present battle scene ???
 
         StartFight(myFighter0, myFighter1);
+
+        //Reset scene to strategy mode
     }
 
     //Start battle sequence. Kapow.
     void StartFight(Fighter aFighter0, Fighter aFighter1)
     {
-        //myFighter0 hits opponent and switch active fighter
-        //myFighter1 hits opponent and switch active fighter ???
-        //Reset scene to strategy mode
-    }
-
-    //Active makes attack on inactive Fighter, then active fighter is switched
-    void MakeAttack(Fighter aActiveFighter, Fighter aInactiveFighter)
-    {
         float damageRate = 1f;
         int attackerTotalPower;
         int defenderTotalDefence;
 
-        if (myActiveFighter == false) //Active fighter = 0
+        attackerTotalPower = aActiveFighter.myPower + aActiveFighter.myWeapon.myPower;
+        defenderTotalDefence = aInactiveFighter.myDefence + aInactiveFighter.myArmour.myDefence;
+
+        //Adjust damage rate based on damage and armour types used
+        switch (aActiveFighter.myWeapon.myDamageType)
         {
-            attackerTotalPower = aActiveFighter.myPower + aActiveFighter.myWeapon.myPower;
-            defenderTotalDefence = aInactiveFighter.myDefence + aInactiveFighter.myArmour.myDefence;
-
-            //Adjust damage rate based on damage and armour types used
-            switch (aActiveFighter.myWeapon.myDamageType)
-            {
-                case DamageType.Slashing:
-                    switch (aInactiveFighter.myArmour.myArmourType)
-                    {
-                        case ArmourType.Heavy:
-                            damageRate *= 0.5f;
-                            break;
-                        case ArmourType.Medium:
-                            damageRate *= 1f;
-                            break;
-                        case ArmourType.Light:
-                            damageRate *= 2f;
-                            break;
-                        case ArmourType.Magical:
-                            damageRate *= 1f;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case DamageType.Blunt:
-                    switch (aInactiveFighter.myArmour.myArmourType)
-                    {
-                        case ArmourType.Heavy:
-                            damageRate *= 2f;
-                            break;
-                        case ArmourType.Medium:
-                            damageRate *= 0.5f;
-                            break;
-                        case ArmourType.Light:
-                            damageRate *= 1f;
-                            break;
-                        case ArmourType.Magical:
-                            damageRate *= 1f;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case DamageType.Piercing:
-                    switch (aInactiveFighter.myArmour.myArmourType)
-                    {
-                        case ArmourType.Heavy:
-                            damageRate *= 1f;
-                            break;
-                        case ArmourType.Medium:
-                            damageRate *= 2f;
-                            break;
-                        case ArmourType.Light:
-                            damageRate *= 0.5f;
-                            break;
-                        case ArmourType.Magical:
-                            damageRate *= 1f;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case DamageType.Magical:
-                    damageRate *= 1f;
-                    break;
-                default:
-                    break;
-            }
-
-            //Adjust damage rate based on attack vs defence
-            if (attackerTotalPower >= (2 * defenderTotalDefence))
-            {
-                damageRate *= 2f;
-            }
-            else if (attackerTotalPower <= (2 / defenderTotalDefence))
-            {
-                damageRate *= 0.5f;
-            }
-            else
-            {
+            case DamageType.Slashing:
+                switch (aInactiveFighter.myArmour.myArmourType)
+                {
+                    case ArmourType.Heavy:
+                        damageRate *= 0.5f;
+                        break;
+                    case ArmourType.Medium:
+                        damageRate *= 1f;
+                        break;
+                    case ArmourType.Light:
+                        damageRate *= 2f;
+                        break;
+                    case ArmourType.Magical:
+                        damageRate *= 1f;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case DamageType.Blunt:
+                switch (aInactiveFighter.myArmour.myArmourType)
+                {
+                    case ArmourType.Heavy:
+                        damageRate *= 2f;
+                        break;
+                    case ArmourType.Medium:
+                        damageRate *= 0.5f;
+                        break;
+                    case ArmourType.Light:
+                        damageRate *= 1f;
+                        break;
+                    case ArmourType.Magical:
+                        damageRate *= 1f;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case DamageType.Piercing:
+                switch (aInactiveFighter.myArmour.myArmourType)
+                {
+                    case ArmourType.Heavy:
+                        damageRate *= 1f;
+                        break;
+                    case ArmourType.Medium:
+                        damageRate *= 2f;
+                        break;
+                    case ArmourType.Light:
+                        damageRate *= 0.5f;
+                        break;
+                    case ArmourType.Magical:
+                        damageRate *= 1f;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case DamageType.Magical:
                 damageRate *= 1f;
-            }
-
-            float damageDealt = attackerTotalPower * damageRate;
-
-            //aFigher0 deals damage to aFighter1
-        }
-        else    //Active fighter = 1
-        {
-            myActiveFighter = false;
+                break;
+            default:
+                break;
         }
 
-        if (myActiveFighter == false)
+        //Adjust damage rate based on attack vs defence 
+        if (attackerTotalPower >= (2 * defenderTotalDefence))
         {
-            myActiveFighter = true;
+            damageRate *= 2f;
+        }
+        else if (attackerTotalPower <= (2 / defenderTotalDefence))
+        {
+            damageRate *= 0.5f;
         }
         else
         {
-            myActiveFighter = false;
+            damageRate *= 1f;
         }
+
+        float damageDealt = attackerTotalPower * damageRate;
+
+        //aFigher0 deals damage to aFighter1
     }
 
     // Update is called once per frame
