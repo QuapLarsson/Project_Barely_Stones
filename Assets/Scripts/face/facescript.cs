0e2148a04e243859a9523807b1c3db2d6b55c573
toapapper <toapapper@users.noreply.github.com>
2020-05-27 23:03:29 +0200

    Update facescript.cs

--- Assets/Scripts/face/facescript.cs
+++ Assets/Scripts/face/facescript.cs
@@ -21,8 +21,9 @@ public class facescript : MonoBehaviour
         faceBone = transform.Find(faceBonePath);
 
         activeFace = Instantiate(facePrefab, faceBone);
-
-        activeFace.transform.position = new Vector3(0, 0.0009f, 0.0013f);
+        double temp0 = 0.09d;
+        double temp1 = 0.13d;
+        activeFace.transform.position = new Vector3(0, (float)temp0/100, (float)temp1/100);
     }
 
     void Update()
