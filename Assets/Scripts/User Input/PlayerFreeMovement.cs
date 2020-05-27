36177d575f3f7dc1dba157beaf2014c156054e7c
Celezt <celezt@github.com>
2020-05-27 16:49:35 +0200

    Minor Changes

--- Assets/Scripts/User Input/PlayerFreeMovement.cs
+++ Assets/Scripts/User Input/PlayerFreeMovement.cs
@@ -55,7 +55,6 @@ namespace Barely.UserInput
             {
                 if (PauseManager.pauseState == PauseManager.PauseState.Playing)
                 {
-                    
                     if (Input.GetMouseButtonDown(_mousebutton))
                     {
                         MouseAction();
@@ -65,6 +64,14 @@ namespace Barely.UserInput
                         if (!_executeCoroutine)
                             StartCoroutine(Hold());
                 }
+                if (Input.GetMouseButtonDown(_mousebutton))
+                {
+                    MouseAction();
+                    yield return new WaitForSeconds(0.2f);
+                }
+                else if (Input.GetMouseButton(_mousebutton))
+                    if (!_executeCoroutine)
+                        StartCoroutine(Hold());
 
                 yield return new WaitForFixedUpdate();
             }
