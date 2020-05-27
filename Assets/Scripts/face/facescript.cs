0d9cd9e30bb74a0c7c32460d828d5f4b5ca4e27e
toapapper <toapapper@users.noreply.github.com>
2020-05-27 19:28:07 +0200

    faces03

---
+++ Assets/Scripts/face/facescript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class facescript : MonoBehaviour
+{
+
+    Transform faceBone;
+    public string faceBonePath = "Armature/DEF_hip/DEF_spine.001/DEF_spine.002/DEF_neck/DEF_neck.001/DEF_head";
+
+    Vector3 offset;
+    Vector3 scale;
+    Vector3 rotation;
+
+    GameObject face;
+
+    void Start()
+    {
+        faceBone = transform.Find(faceBonePath);
+    }
+    
+
+
+}
