33a374e8e0a6af69aa8c64319f41a3da998b8b22
toapapper <toapapper@users.noreply.github.com>
2020-05-27 22:50:24 +0200

    faces added and facescript added

--- Assets/Scripts/face/facescript.cs
+++ Assets/Scripts/face/facescript.cs
@@ -6,19 +6,30 @@ public class facescript : MonoBehaviour
 {
 
     Transform faceBone;
-    public string faceBonePath = "Armature/DEF_hip/DEF_spine.001/DEF_spine.002/DEF_neck/DEF_neck.001/DEF_head";
+    public string faceBonePath = "Armature/DEF_hip/DEF_spine/DEF_spine.001/DEF_spine.002/DEF_neck/DEF_neck.001/DEF_head";
+    
+    [SerializeField]
+    GameObject facePrefab;
 
-    Vector3 offset;
-    Vector3 scale;
-    Vector3 rotation;
+    GameObject activeFace;
 
-    GameObject face;
+
+
 
     void Start()
     {
         faceBone = transform.Find(faceBonePath);
-    }
-    
 
+        activeFace = Instantiate(facePrefab, faceBone);
+
+        activeFace.transform.position = new Vector3(0, 0.0009f, 0.0013f);
+    }
+
+    void Update()
+    {
+
+        //TÄnkte lägga till ett byta till combat_face här men det kan man ta senare kanske
+
+    }
 
 }
